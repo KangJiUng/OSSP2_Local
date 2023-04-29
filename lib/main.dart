a14fb23d5903b72493996080e1a722fb83550cfd
UNG26 <woong236406@gmail.com>
2023-04-29 14:42:03 +0900

    안드로이드스튜디오 연동 테스트~

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('>'),),
+        body: Text('식단 및 운동 루틴 입력'),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            height: 70,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page),
+              ],
+            ),
+          )
+        ),
+      )
+    );
+  }
+}
